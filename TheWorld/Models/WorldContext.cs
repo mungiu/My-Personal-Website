366ee36b436141e0de7276e459bf5642fec66f20
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-29 11:31:40 +0200

    Database created & connection established

---
+++ TheWorld/Models/WorldContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Models
+{
+    //database related
+    public class WorldContext : DbContext
+    {
+        private IConfigurationRoot _config;
+
+        //ctor
+        public WorldContext(IConfigurationRoot config, DbContextOptions options) 
+            : base(options)
+        {
+            _config = config;
+        }
+
+        //classes that we can execute LINQ queries against
+        //these are starting points for queriable interfaces
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Stop> Stops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:WorldContextConnection"]);
+        }
+    }
+}
